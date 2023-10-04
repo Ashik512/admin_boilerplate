d4a67f55e15cc38f241860b198a94e6ba11868a8
Ashik512 <ashikiqbal.cse@gmail.com>
2023-10-04 17:11:53 +0600

    modified some

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,34 +4,34 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <>
-      <div className={'p-2 space-y-2'}>
+      <div className={'p-2'}>
         <Row gutter={16}>
-          <Col md={6}>
-            <Card className="bg-red-300 text-white">
+          <Col xs={24} md={6} className='sm:mb-3'>
+            <Card className="text-white" style={{background: '#4908FA'}}>
               <div className="flex justify-between">
                 <span>Subject</span>
                 <span>10</span>
               </div>
             </Card>
           </Col>
-          <Col md={6}>
-            <Card className="bg-orange-300 text-white ">
+          <Col xs={24} md={6} className='sm:mb-3'>
+            <Card className="text-white" style={{background: '#06D6A0'}}>
               <div className="flex justify-between">
                 <span>Topic</span>
                 <span>20</span>
               </div>
             </Card>
           </Col>
-          <Col md={6}>
-            <Card className="bg-slate-300 text-white">
+          <Col xs={24} md={6} className='sm:mb-3'>
+            <Card className="text-white" style={{background: '#F2385A'}}>
               <div className="flex justify-between">
                 <span>Question</span>
                 <span>30</span>
               </div>
             </Card>
           </Col>
-          <Col md={6}>
-            <Card className="bg-blue-300 text-white">
+          <Col xs={24} md={6} className='sm:mb-3'>
+            <Card className="text-white" style={{background: '#444444'}}>
               <div className="flex justify-between">
                 <span>Candidates</span>
                 <span>90</span>
